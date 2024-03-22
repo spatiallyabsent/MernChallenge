bcafa8e0faeafdcd03fc4920a5c5358301608c38
SpatiallyAbsent <joshuapdow@gmail.com>
2024-03-22 13:03:12 -0600

    added strategy to create query and mutations in resolvers.js

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,1 +1,14 @@
 //TODO add resolvers for all the schemas
+//use examples from MERN exercise 22
+// pull in the models and functions from utils
+const { User, Book } = require('../models');
+const { signToken, authMiddleware } = require('../utils/auth');
+// will need the following resolvers:
+//Query
+  //getSingleUser
+//Mutation
+  //createUser
+  //login
+  //saveBook
+  //deleteBook
+
