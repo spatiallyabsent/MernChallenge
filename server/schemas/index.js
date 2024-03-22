9169a2d039c5dea0ebce7786c55f8d9d799deff8
SpatiallyAbsent <joshuapdow@gmail.com>
2024-03-22 12:32:57 -0600

    changed server.js to connect with Graphql and let Apollo manage routes

--- server/schemas/index.js
+++ server/schemas/index.js
@@ -1,1 +1,5 @@
 //TODO add and export typedefs and resolvers
+const typeDefs = require('./typeDefs');
+const resolvers = require('./resolvers');
+
+module.exports = { typeDefs, resolvers };
--- server/server.js
+++ server/server.js
@@ -1,22 +1,39 @@
-//TODO add middleware for apollo and graphql
+//TODO add middleware for apollo and graphql use 19 and 20 exercises in MERN for reference
 const express = require('express');
+const { ApolloServer } = require('@apollo/server');//added by me
+const { expressMiddleware } = require('apollo/server/express4');//added by me
 const path = require('path');
-const db = require('./config/connection');
-const routes = require('./routes');
+const routes = require('./routes');//may not need this
 
-const app = express();
+const { typeDefs, resolvers } = require('./schemas');//added by me
+const db = require('./config/connection');
+
 const PORT = process.env.PORT || 3001;
+const app = express();
+const server = new ApolloServer({ typeDefs, resolvers });//added by me
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
+//to use routes for graphql and apollo
+const startApolloServer = async () => {
+  await server.start();
+  // if we're in production, serve client/build as static assets
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+    //added by me for redirecting to index.html
+    app.get('*', (req, res) => {
+      res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+    });
+  }
 
-app.use(routes);
+  app.use('/graphql', expressMiddleware({ server }));//added by me to switch to graphql
 
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-});
+  db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+      console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
+    });
+  });
+};
